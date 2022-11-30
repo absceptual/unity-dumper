a3d826acf218887d73c8ae4b2af35b9cba7388fe
abs <absceptual@gmail.com>
2022-11-29 20:01:17 -0800

    Update dumper.hpp

--- src/dumper/dumper.hpp
+++ src/dumper/dumper.hpp
@@ -1,31 +1,39 @@
+#pragma once
 #include <algorithm>
 #include <cstdint>
 #include <string>  
 #include <vector>
 
 #include <src/dumper/images.hpp>
-#include <src/dumper/api.hpp>
 
 class Dumper
 {
-private:
-    images_t m_images { };
-    
 public:
-    using images_t = std::vector<Image*>;
+    using images_t  = std::vector<Image*>;
+private:
+    images_t  m_images { };
+    Image::classes_t m_classes { };
+public:
     
     // Dumps all images on initalization
-    Dumper() 
+    Dumper(bool use_global_class_table = false)
     {
         size_t count = 0U;
         const auto domain = api::get_domain();
         const auto assemblies = api::get_assemblies(domain, &count);
-      
-        for (size_t index = 0U; index < count; ++index) 
+
+        for (size_t index = 0U; index < count; ++index)
         {
-            m_images.emplace_back(api::get_image(assemblies[index]));
+            auto image = api::get_image(assemblies[index]);
+            m_images.emplace_back(static_cast< Image* >(image));
+
+            if (use_global_class_table)
+            {
+               for (const auto klass : image->get_classes())
+                    m_classes.emplace_back(klass);
+            }
         }
-    
+
     };
 
     // Loop through current image names and checks their names to find a matching image
@@ -33,11 +41,15 @@ public:
     {
         for (const auto image : m_images)
         {
-            if (!strcmp(name, image->get_name())
+            if (!strcmp(name, image->get_name()))
                 return image;
         }
         return nullptr;
     }
-                
+
+    // Returns image table
     auto get_images() const -> images_t { return m_images; }
+
+    // Returns all global classes
+    auto get_all_classes() const -> Image::classes_t { return m_classes; }
 };
